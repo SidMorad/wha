7ab850115329ffa9c37b56e40fef4f98ee13f38c
behrooz.nobakht <behrooz.nobakht@gmail.com>
2009-12-03 00:46:48 +0000

    This is called Business Level Security. Actually no framework would implement it for us. Still, something like Spring Security ACL would believe to do but .... So, now I'd suppose this would be done. There would be some architectural views on this also remained for refactoring phases.

--- src/main/java/nl/hajari/wha/web/controller/TimeController.java
+++ src/main/java/nl/hajari/wha/web/controller/TimeController.java
@@ -1,14 +1,19 @@
 package nl.hajari.wha.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import nl.hajari.wha.domain.DailyTimesheet;
 import nl.hajari.wha.domain.Project;
 import nl.hajari.wha.domain.Timesheet;
+import nl.hajari.wha.domain.User;
+import nl.hajari.wha.web.util.SecurityContextUtils;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
@@ -21,34 +26,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/time/**")
 @Controller
 public class TimeController {
-	
+
 	protected final Log logger = LogFactory.getLog(getClass());
 
 	@Autowired
 	TimesheetController timesheetController;
-	
+
 	@RequestMapping(value = "/time/daily", method = RequestMethod.POST)
 	@Transactional
 	public String createDailyTimesheet(@Valid DailyTimesheet dailyTimesheet, BindingResult result, ModelMap modelMap) {
-		if (dailyTimesheet == null) { throw new IllegalArgumentException("A dailyTimesheet is required"); }
+		if (dailyTimesheet == null) {
+			throw new IllegalArgumentException("A dailyTimesheet is required");
+		}
 
 		if (!result.hasErrors()) {
-			Timesheet timesheet = timesheetController.getTimesheetByDayDateAndCurrentEmployee(dailyTimesheet.getDayDate());
+			Timesheet timesheet = timesheetController.getTimesheetByDayDateAndCurrentEmployee(dailyTimesheet
+					.getDayDate());
 			if (timesheet == null) {
-				// we the correct settings we shouldn't see this message , but we have it in case .
-				result.rejectValue("dayDate" ,"error.time.timesheet.not.avaiable");
+				// we the correct settings we shouldn't see this message , but
+				// we have it in case .
+				result.rejectValue("dayDate", "error.time.timesheet.not.avaiable");
 			} else {
 				dailyTimesheet.setTimesheet(timesheet);
 			}
-		}	
-		
+		}
+
 		if (result.hasErrors()) {
 			modelMap.addAttribute("projects", Project.findAllProjects());
 			return "time/daily/create";
 		}
-		
-		// calculate dailyTotalDuration = duration + durationTraining - durationOffs
-		dailyTimesheet.setDailyTotalDuration(dailyTimesheet.getDuration() + dailyTimesheet.getDurationTraining() - dailyTimesheet.getDurationOffs());
+
+		// calculate dailyTotalDuration = duration + durationTraining -
+		// durationOffs
+		dailyTimesheet.setDailyTotalDuration(dailyTimesheet.getDuration() + dailyTimesheet.getDurationTraining()
+				- dailyTimesheet.getDurationOffs());
 		dailyTimesheet.persist();
 		dailyTimesheet.flush();
 		// now we update monthlyTotalDuration in timesheet entity
@@ -56,10 +67,30 @@ public class TimeController {
 		Timesheet.updateTimesheetTotalMonthly(timesheetId, DailyTimesheet.findTimesheetTotalMonthly(timesheetId));
 		return "redirect:/time/daily/" + dailyTimesheet.getId();
 	}
-	
-   @InitBinder    
-    public void initBinder(WebDataBinder binder) {    
-        binder.registerCustomEditor(java.util.Date.class, new org.springframework.beans.propertyeditors.CustomDateEditor(new java.text.SimpleDateFormat("d/MM/yy"), true));        
-   }
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(java.util.Date.class,
+				new org.springframework.beans.propertyeditors.CustomDateEditor(
+				new java.text.SimpleDateFormat("d/MM/yy"), true));
+	}
+
+	public boolean authorizeAccessTimesheet(Long id, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		Timesheet timesheet = Timesheet.findTimesheet(id);
+		if (timesheet == null) {
+			return true;
+		}
+		Long userId = timesheet.getEmployee().getUser().getId();
+		User user = (User) User.findUsersByUsernameEquals(SecurityContextUtils.getCurrentUsername()).getSingleResult();
+		if (user == null || !user.getId().equals(userId)) {
+			String message = "Unauthorized access of user [" + user + "] to timesheet[" + timesheet + "] for employee["
+					+ timesheet.getEmployee() + "]";
+			logger.warn(message);
+//			response.sendError(HttpServletResponse.SC_FORBIDDEN, message);
+			throw new AccessDeniedException(message);
+		}
+		return true;
+	}
 
 }
--- src/main/java/nl/hajari/wha/web/controller/TimeController_TimesheetController.aj
+++ src/main/java/nl/hajari/wha/web/controller/TimeController_TimesheetController.aj
@@ -1,6 +1,7 @@
 package nl.hajari.wha.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import nl.hajari.wha.domain.Employee;
 import nl.hajari.wha.domain.Timesheet;
@@ -12,26 +13,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 privileged aspect TimeController_TimesheetController {
-	
+
 	@RequestMapping(value = "/time/timesheet", method = RequestMethod.GET)
-	public String TimeController.listTimesheet(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap, HttpServletRequest request) {
+	public String TimeController.listTimesheet(@RequestParam(value = "page", required = false) Integer page,
+			@RequestParam(value = "size", required = false) Integer size, ModelMap modelMap, HttpServletRequest request) {
 		Long employeeId = (Long) request.getSession().getAttribute(Employee.EMPLOYEE_ID);
 		if (page != null || size != null) {
 			int sizeNo = size == null ? 10 : size.intValue();
-			modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByEmployeeId(employeeId, page == null ? 0 : (page.intValue() -1) * sizeNo, sizeNo));
+			modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByEmployeeId(employeeId, page == null ? 0
+					: (page.intValue() - 1) * sizeNo, sizeNo));
 			float nrOfPages = (float) Timesheet.countTimesheets() / sizeNo;
-			modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+			modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1
+					: nrOfPages));
 		} else {
 			modelMap.addAttribute("timesheets", Timesheet.findAllTimesheetsByEmployeeId(employeeId));
 		}
 		return "time/timesheet/list";
 	}
-	
+
 	@RequestMapping(value = "/time/timesheet/{id}", method = RequestMethod.GET)
-	public String TimeController.showTimesheet(@PathVariable("id") Long id, ModelMap modelMap) {
-		if (id == null) { throw new IllegalArgumentException("An Identifer is required");}
+	public String TimeController.showTimesheet(@PathVariable("id") Long id, HttpServletRequest request,
+			HttpServletResponse response, ModelMap modelMap) throws Exception {
+		if (id == null) {
+			throw new IllegalArgumentException("An Identifer is required");
+		}
+		authorizeAccessTimesheet(id, request, response);
 		modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));
 		return "time/timesheet/show";
 	}
-	
+
 }
