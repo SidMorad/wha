e10dcff484f8b70bb40ae073f1cbb33dfd143c29
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2009-12-04 09:05:49 +0000

    Some small refactors .

--- trunk/src/main/java/nl/hajari/wha/web/controller/TimesheetController.java
+++ trunk/src/main/java/nl/hajari/wha/web/controller/TimesheetController.java
@@ -1,14 +1,15 @@
 package nl.hajari.wha.web.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import nl.hajari.wha.domain.Employee;
 import nl.hajari.wha.domain.Timesheet;
 import nl.hajari.wha.domain.User;
+import nl.hajari.wha.web.util.DateUtils;
+import nl.hajari.wha.web.util.SecurityContextUtils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,17 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TimesheetController {
 
-	public Timesheet getTimesheetByDayDateAndCurrentEmployee(Date dayDate) {
-		User currentUser = (User) User.findUsersByUsernameEquals(SecurityContextHolder.getContext().getAuthentication().getName()).getSingleResult(); 
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	public Timesheet getTimesheetOrCreateOneForCurrentEmployee() {
+		User currentUser = (User) User.findUsersByUsernameEquals(SecurityContextUtils.getCurrentUsername()).getSingleResult(); 
 		Employee currentEmployee = (Employee) Employee.findEmployeesByUser(currentUser).getSingleResult();
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(dayDate);
-		int sheetYear = calendar.get(Calendar.YEAR);
-		int sheetMonth = calendar.get(Calendar.MONTH);
+		int sheetYear = DateUtils.getCurrentYear();
+		int sheetMonth = DateUtils.getCurrentMonth();
 		Timesheet timesheet = new Timesheet();
 		try {
 			timesheet = (Timesheet) Timesheet.findTimesheetsByEmployeeAndSheetMonthAndSheetYearEquals(currentEmployee, sheetMonth, sheetYear).getSingleResult();
-		} catch (Exception e) {
+		} catch (DataAccessException dae) {
+			logger.debug("Looks like there is no timesheet for this employee[" + currentEmployee + "] , " +
+					"let's create one in [" + sheetYear + " / " + sheetMonth + "]");
 			// if timesheet doesn't exist make one. 
 			timesheet.setSheetYear(sheetYear);
 			timesheet.setSheetMonth(sheetMonth);
