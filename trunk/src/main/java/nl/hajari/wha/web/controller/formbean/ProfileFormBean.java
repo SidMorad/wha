c5609860fe9f9007e377687e540bf8cd75e0a26f
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2009-12-26 08:20:58 +0000

    Issue 52:  	 Task: Profile page for Employee (name/email/password fields are required)
     Change FirstName/LastName with Password/ConfirmPassword.

--- trunk/src/main/java/nl/hajari/wha/web/controller/formbean/ProfileFormBean.java
+++ trunk/src/main/java/nl/hajari/wha/web/controller/formbean/ProfileFormBean.java
@@ -17,17 +17,18 @@ public class ProfileFormBean {
 	private Integer version;
 
 	@NotNull
-	@Size(min = 2, max = 45)
-	private String firstName;
-	@NotNull
-	@Size(min = 2, max = 45)
-	private String lastName;
-
-	@NotNull
 	@Size(max = 30)
 	@Pattern(regexp = ".+@.+\\.[a-z]+")
 	private String email;
 
+	@NotNull
+	@Size(min = 4 , max = 30)
+	private String password;
+
+	@NotNull
+	@Size(min = 4 , max = 30)
+	private String confirmPassword;
+	
 	public String getEmail() {
 		return email;
 	}
@@ -52,20 +53,19 @@ public class ProfileFormBean {
 		this.version = version;
 	}
 
-	public String getLastName() {
-		return lastName;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public String getConfirmPassword() {
+		return confirmPassword;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
-
 }
