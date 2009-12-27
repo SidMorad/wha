3ea1e670be3c991d03ed1002ae6c9354a875a083
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2009-12-27 08:57:26 +0000

    Issue 9:  	 Architectural Risks: Report Generation
    * Integrate Jasperreports 
    * Add maven-jasperreports-plugin 
    * Provide simple example (customer list) .
    
    Note: Customer list example is not complete yet, we only need to pass JRBeanCollectionDataSource to the report file.

--- trunk/src/main/java/nl/hajari/wha/web/controller/CustomerController.java
+++ trunk/src/main/java/nl/hajari/wha/web/controller/CustomerController.java
@@ -1,12 +1,27 @@
 package nl.hajari.wha.web.controller;
 
-import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import nl.hajari.wha.domain.Customer;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RooWebScaffold(path = "customer", automaticallyMaintainView = false, formBackingObject = Customer.class)
 @RequestMapping("/customer/**")
 @Controller
 public class CustomerController {
+
+	@RequestMapping(value ="/customer/report/pdf", method = RequestMethod.GET)
+	public String fireReport(ModelMap modelMap) {
+		JRBeanCollectionDataSource jrDataSource = new JRBeanCollectionDataSource(Customer.findAllCustomers());
+		//TODO: replace emptyDataSource with beanCollectionDataSource
+		modelMap.put("customerListData", new JREmptyDataSource());
+		
+		return "customerList";
+	}
+
 }
