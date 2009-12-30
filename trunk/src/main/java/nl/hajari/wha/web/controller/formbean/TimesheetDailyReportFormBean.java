dae1dfc8b68d1fb6519b6da973471dafe7f479f0
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2009-12-30 03:50:20 +0000

    Issue 56:  	 Task: Reproting monthly timesheet.
    * Change JasperReportPdfFormatView to JasperReportMultiFormatView (pdf, csv, xls and html).

--- trunk/src/main/java/nl/hajari/wha/web/controller/formbean/TimesheetDailyReportFormBean.java
+++ trunk/src/main/java/nl/hajari/wha/web/controller/formbean/TimesheetDailyReportFormBean.java
@@ -1,5 +1,7 @@
 package nl.hajari.wha.web.controller.formbean;
 
+import javax.validation.constraints.NotNull;
+
 import nl.hajari.wha.domain.Customer;
 
 /**
@@ -10,9 +12,14 @@ import nl.hajari.wha.domain.Customer;
  **/
 public class TimesheetDailyReportFormBean {
 
+	@NotNull
 	private Long timesheetId;
-	
+
+	@NotNull
 	private Customer customer;
+
+	@NotNull
+	private String format;
 
 	public Long getTimesheetId() {
 		return timesheetId;
@@ -28,5 +35,13 @@ public class TimesheetDailyReportFormBean {
 
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
 	}
 }
