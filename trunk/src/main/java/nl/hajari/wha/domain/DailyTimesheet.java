a9479cfff5ff4103ef9cfdea24c80b1582d58a1c
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2010-01-08 02:23:19 +0000

    Issue 56:  	 Task: Reproting monthly timesheet.
    * DailyTimesheet report per project for ROLE_EMPLOYEE.

--- trunk/src/main/java/nl/hajari/wha/domain/DailyTimesheet.java
+++ trunk/src/main/java/nl/hajari/wha/domain/DailyTimesheet.java
@@ -16,45 +16,60 @@ import org.springframework.roo.addon.javabean.RooJavaBean;
 
 @Entity
 @RooJavaBean
-@RooEntity(finders = { "findDailyTimesheetsByTimesheet", "findDailyTimesheetsByDayDateAndTimesheet" })
+@RooEntity(finders = { "findDailyTimesheetsByTimesheet",
+		"findDailyTimesheetsByDayDateAndTimesheet" })
 public class DailyTimesheet {
 
-    @NotNull
-    @Temporal(TemporalType.DATE)
-    @Column(nullable = false)
-    private Date dayDate;
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
+	private Date dayDate;
 
-    @NotNull
-    @Column(nullable = false)
-    private Float duration;
+	@NotNull
+	@Column(nullable = false)
+	private Float duration;
 
-    @NotNull
-    @Column(nullable = false)
-    private Float durationOffs;
+	@NotNull
+	@Column(nullable = false)
+	private Float durationOffs;
 
-    @NotNull
-    @Column(nullable = false)
-    private Float durationTraining;
+	@NotNull
+	@Column(nullable = false)
+	private Float durationTraining;
 
-    @Column(nullable = false)
-    private Float dailyTotalDuration;
+	@Column(nullable = false)
+	private Float dailyTotalDuration;
 
-    @ManyToOne(targetEntity = Timesheet.class)
-    @JoinColumn(name = "timesheet_id", nullable = false)
-    private Timesheet timesheet;
+	@ManyToOne(targetEntity = Timesheet.class)
+	@JoinColumn(name = "timesheet_id", nullable = false)
+	private Timesheet timesheet;
 
-    @ManyToOne(targetEntity = Project.class)
-    @JoinColumn(name = "project_id", nullable = false)
-    private Project project;
-    
-    private transient String projectName;
+	@ManyToOne(targetEntity = Project.class)
+	@JoinColumn(name = "project_id", nullable = false)
+	private Project project;
 
-    public String toString() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(dayDate);
-        int dayInt = calendar.get(Calendar.DAY_OF_MONTH);
-        String weekDay = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
-        String month = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault());
-        return weekDay + " " + month + " " + dayInt + " > " + duration;
-    }
+	private transient String projectName;
+
+	public DailyTimesheet() {
+	}
+
+	public DailyTimesheet(Timesheet timesheet, Float duration, Float offs, Float training,
+			Float total) {
+		this.timesheet = timesheet;
+		this.duration = duration;
+		this.durationOffs = offs;
+		this.durationTraining = training;
+		this.dailyTotalDuration = total;
+	}
+
+	public String toString() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dayDate);
+		int dayInt = calendar.get(Calendar.DAY_OF_MONTH);
+		String weekDay = calendar.getDisplayName(Calendar.DAY_OF_WEEK,
+				Calendar.LONG, Locale.getDefault());
+		String month = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT,
+				Locale.getDefault());
+		return weekDay + " " + month + " " + dayInt + " > " + duration;
+	}
 }
