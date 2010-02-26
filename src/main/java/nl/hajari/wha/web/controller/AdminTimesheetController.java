a6e8ff2bff134d53e6b1e82d43376d11141f10ed
sid.moradi <sid.moradi@4690526c-c394-11de-8745-9f8d1d7459b8>
2010-02-26 03:17:16 +0000

    Issue 92:  	 Admin Panel: List All Time Sheets
    * Add Form at top of timesheet list
    
    Note for behrooz : I've use 'Mark as merged' in some files , please check if I made mistacke , thanks :)

--- src/main/java/nl/hajari/wha/web/controller/AdminTimesheetController.java
+++ src/main/java/nl/hajari/wha/web/controller/AdminTimesheetController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import nl.hajari.wha.Constants;
@@ -15,12 +16,17 @@ import nl.hajari.wha.service.impl.DailyExpenseServiceImpl;
 import nl.hajari.wha.service.impl.DailyTimesheetServiceImpl;
 import nl.hajari.wha.service.impl.ProjectServiceImpl;
 import nl.hajari.wha.web.controller.formbean.TimesheetDailyReportFormBean;
+import nl.hajari.wha.web.controller.formbean.TimesheetYearMonthFormBean;
 import nl.hajari.wha.web.util.DateUtils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +34,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminTimesheetController extends AbstractController {
+	
+	protected final Log logger = LogFactory.getLog(getClass());
 
 	@Autowired
 	protected DailyTimesheetServiceImpl dailyTimesheetService;
@@ -37,53 +45,71 @@ public class AdminTimesheetController extends AbstractController {
 
 	@Autowired
 	protected DailyExpenseServiceImpl dailyExpenseService;
-
+	
 	@Autowired
 	protected TimesheetService timesheetService;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(java.util.Date.class,
+				new org.springframework.beans.propertyeditors.CustomDateEditor(
+				new java.text.SimpleDateFormat("d/MM/yy"), true));
+	}	
+	
+    @RequestMapping(value = "/admin/timesheet", method = RequestMethod.GET)    
+    public String listTimesheetAll(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {    
+        if (page != null || size != null) {        
+            int sizeNo = size == null ? 10 : size.intValue();            
+            modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByYearAndMonth(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo, null, null));            
+            float nrOfPages = (float) Timesheet.countTimesheets() / sizeNo;            
+            modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));            
+        } else {
+        	// -1 will ignore pagination and null will replace with current Year and Month
+            modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByYearAndMonth(-1,-1,null,null));            
+        }
+        modelMap.addAttribute("timesheetYearMonthFormBean", new TimesheetYearMonthFormBean(DateUtils.getCurrentYear(), DateUtils.getCurrentMonth()));
+        return "admin/timesheet/list";        
+    }   
 
-	@RequestMapping(value = "/admin/timesheet", method = RequestMethod.GET)
-	public String listTimesheetAll(@RequestParam(value = "page", required = false) Integer page,
-			@RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {
-		if (page != null || size != null) {
-			int sizeNo = size == null ? 10 : size.intValue();
-			modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntries(page == null ? 0 : (page.intValue() - 1)
-					* sizeNo, sizeNo));
-			float nrOfPages = (float) Timesheet.countTimesheets() / sizeNo;
-			modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1
-					: nrOfPages));
-		} else {
-			modelMap.addAttribute("timesheets", Timesheet.findAllTimesheets());
-		}
-		return "admin/timesheet/list";
-	}
-
-	@RequestMapping(value = "/admin/timesheet/daily/{id}", method = RequestMethod.GET)
-	public String showTimesheetDaily(@PathVariable("id") Long id, ModelMap modelMap) {
-		if (id == null)
-			throw new IllegalArgumentException("An Identifier is required");
-		modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));
-		modelMap.addAttribute("timesheetDailyReportFormBean", new TimesheetDailyReportFormBean());
-		modelMap.addAttribute("customers", Customer.findAllCustomers());
-		return "admin/timesheet/daily/show";
-	}
-
-	@RequestMapping(value = "/admin/timesheet/travel/{id}", method = RequestMethod.GET)
-	public String showTimesheetTravel(@PathVariable("id") Long id, ModelMap modelMap) {
-		if (id == null)
-			throw new IllegalArgumentException("An Identifier is required");
-		modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));
-		return "admin/timesheet/travel/show";
-	}
-
-	@RequestMapping(value = "/admin/timesheet/expense/{id}", method = RequestMethod.GET)
-	public String showTimesheetExpense(@PathVariable("id") Long id, ModelMap modelMap) {
-		if (id == null)
-			throw new IllegalArgumentException("An Identifier is required");
-		modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));
-		return "admin/timesheet/expense/show";
-	}
-
-	@RequestMapping(value = "/admin/timesheet/ponumber/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/admin/timesheet/refresh", method = RequestMethod.POST) 
+    public String listTimesheetByYearAndMonth(@Valid TimesheetYearMonthFormBean yearMonthFormBean, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, ModelMap modelMap) {
+    	
+    	if (page != null || size != null) {        
+    		int sizeNo = size == null ? 10 : size.intValue();            
+    		modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByYearAndMonth(page == null ? 0 : (page.intValue() - 1) * sizeNo, sizeNo, yearMonthFormBean.getYear(), yearMonthFormBean.getMonth()));            
+    		float nrOfPages = (float) Timesheet.countTimesheets() / sizeNo;            
+    		modelMap.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));            
+    	} else {
+    		// -1 will ignore pagination and null will replace with current Year and Month
+    		modelMap.addAttribute("timesheets", Timesheet.findTimesheetEntriesByYearAndMonth(-1,-1,yearMonthFormBean.getYear(), yearMonthFormBean.getMonth()));            
+    	}        
+    	return "admin/timesheet/list";        
+    }
+	
+    @RequestMapping(value = "/admin/timesheet/daily/{id}", method = RequestMethod.GET)    
+    public String showTimesheetDaily(@PathVariable("id") Long id, ModelMap modelMap) {    
+        if (id == null) throw new IllegalArgumentException("An Identifier is required");        
+        modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));
+        modelMap.addAttribute("timesheetDailyReportFormBean", new TimesheetDailyReportFormBean());
+        modelMap.addAttribute("customers", Customer.findAllCustomers());
+        return "admin/timesheet/daily/show";        
+    }   
+    
+    @RequestMapping(value = "/admin/timesheet/travel/{id}", method = RequestMethod.GET)    
+    public String showTimesheetTravel(@PathVariable("id") Long id, ModelMap modelMap) {    
+    	if (id == null) throw new IllegalArgumentException("An Identifier is required");        
+    	modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));        
+    	return "admin/timesheet/travel/show";        
+    }
+    
+    @RequestMapping(value = "/admin/timesheet/expense/{id}", method = RequestMethod.GET)    
+    public String showTimesheetExpense(@PathVariable("id") Long id, ModelMap modelMap) {    
+    	if (id == null) throw new IllegalArgumentException("An Identifier is required");        
+    	modelMap.addAttribute("timesheet", Timesheet.findTimesheet(id));        
+    	return "admin/timesheet/expense/show";        
+    }   
+    
+    @RequestMapping(value = "/admin/timesheet/ponumber/{id}", method = RequestMethod.GET)
 	public String showTimesheetPoNumberForm(@PathVariable("id") Long id, ModelMap modelMap) {
 		if (null == id) {
 			throw new IllegalArgumentException("Timesheet ID is required");
@@ -152,10 +178,9 @@ public class AdminTimesheetController extends AbstractController {
 
 		return "timesheetInvoiceList";
 	}
-
+   
 	public String getFileFullPath(HttpServletRequest request, String filePath) {
 		String appServerHome = request.getSession().getServletContext().getRealPath("/");
 		return appServerHome + filePath;
 	}
-
 }
